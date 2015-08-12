5a5b5ef2bb0a245a0f63262dac2557b471c30582
Ole <ole@vprsm.de>
2015-08-13 01:30:00 +0200

    Extend CEMI dissection further

--- lib/knxclient.cpp
+++ lib/knxclient.cpp
@@ -14,7 +14,21 @@ static
 Local<Object> knxclient_tpdu_to_object(Isolate* isolate, const knx_tpdu& tpdu) {
 	Local<Object> ret_object = Object::New(isolate);
 
+	knxclient_object_register(isolate, ret_object, "tpci", tpdu.tpci);
+	knxclient_object_register(isolate, ret_object, "seq_number", tpdu.seq_number);
 
+	switch (tpdu.tpci) {
+		case KNX_TPCI_UNNUMBERED_DATA:
+		case KNX_TPCI_NUMBERED_DATA:
+			knxclient_object_register(isolate, ret_object, "apci", tpdu.info.data.apci);
+			knxclient_object_register(isolate, ret_object, "payload", knxclient_data_to_buffer(isolate, (const char*) tpdu.info.data.payload, tpdu.info.data.length));
+			break;
+
+		case KNX_TPCI_UNNUMBERED_CONTROL:
+		case KNX_TPCI_NUMBERED_CONTROL:
+			knxclient_object_register(isolate, ret_object, "control", tpdu.info.control);
+			break;
+	}
 
 	return ret_object;
 }
@@ -36,8 +50,7 @@ void knxclient_parse_cemi(const FunctionCallbackInfo<Value>& args) {
 			Local<Object> frame_object = Object::New(isolate);
 
 			// Header
-			frame_object->Set(String::NewFromUtf8(isolate, "service"),
-			                  Integer::New(isolate, frame.service));
+			knxclient_object_register(isolate, frame_object, "service", frame.service);
 
 			// Payload
 			Local<Object> ldata_object = Object::New(isolate);
@@ -47,17 +60,13 @@ void knxclient_parse_cemi(const FunctionCallbackInfo<Value>& args) {
 			knxclient_object_register(isolate, ldata_object, "system_broadcast", frame.payload.ldata.control1.system_broadcast);
 			knxclient_object_register(isolate, ldata_object, "request_ack", frame.payload.ldata.control1.request_ack);
 			knxclient_object_register(isolate, ldata_object, "error", frame.payload.ldata.control1.error);
-
 			knxclient_object_register(isolate, ldata_object, "address_type", frame.payload.ldata.control2.address_type);
 			knxclient_object_register(isolate, ldata_object, "hops", frame.payload.ldata.control2.hops);
-
 			knxclient_object_register(isolate, ldata_object, "source", frame.payload.ldata.source);
 			knxclient_object_register(isolate, ldata_object, "destination", frame.payload.ldata.destination);
+			knxclient_object_register(isolate, ldata_object, "tpdu", knxclient_tpdu_to_object(isolate, frame.payload.ldata.tpdu));
 
-			ldata_object->Set(String::NewFromUtf8(isolate, "tpdu"),
-			                  knxclient_tpdu_to_object(isolate, frame.payload.ldata.tpdu));
-
-			frame_object->Set(String::NewFromUtf8(isolate, "payload"), ldata_object);
+			knxclient_object_register(isolate, frame_object, "payload", ldata_object);
 
 			args.GetReturnValue().Set(frame_object);
 		} else {
