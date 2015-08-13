41a237f44c8e7c3cdc4ec7dd5a6adb8c4247b500
Ole <ole@vprsm.de>
2015-08-13 02:29:08 +0200

    Implement APDU parsing

--- lib/knxclient.cpp
+++ lib/knxclient.cpp
@@ -11,75 +11,6 @@ extern "C" {
 using namespace v8;
 
 static
-Local<Object> knxclient_tpdu_to_object(Isolate* isolate, const knx_tpdu& tpdu) {
-	ObjectBuilder builder(isolate);
-
-	builder.set("tpci", tpdu.tpci);
-	builder.set("seq_number", tpdu.seq_number);
-
-	switch (tpdu.tpci) {
-		case KNX_TPCI_UNNUMBERED_DATA:
-		case KNX_TPCI_NUMBERED_DATA:
-			builder.set("apci", tpdu.info.data.apci);
-			builder.set("payload", ObjectBuilder::fromData(isolate, (const char*) tpdu.info.data.payload, tpdu.info.data.length));
-			break;
-
-		case KNX_TPCI_UNNUMBERED_CONTROL:
-		case KNX_TPCI_NUMBERED_CONTROL:
-			builder.set("control", tpdu.info.control);
-			break;
-	}
-
-	return builder;
-}
-
-static
-void knxclient_parse_cemi(const FunctionCallbackInfo<Value>& args) {
-	Isolate* isolate = args.GetIsolate();
-
-	if (args.Length() > 0 && node::Buffer::HasInstance(args[0])) {
-		Handle<Value> value = args[0];
-
-		// Extract buffer details
-		const uint8_t* data = (const uint8_t*) node::Buffer::Data(value);
-		size_t len = node::Buffer::Length(value);
-
-		// Parse CEMI
-		knx_cemi_frame frame;
-		if (knx_cemi_parse(data, len, &frame)) {
-
-			// L_Data payload
-			ObjectBuilder ldata_builder(isolate);
-			ldata_builder.set("priority", frame.payload.ldata.control1.priority);
-			ldata_builder.set("repeat", frame.payload.ldata.control1.repeat);
-			ldata_builder.set("system_broadcast", frame.payload.ldata.control1.system_broadcast);
-			ldata_builder.set("request_ack", frame.payload.ldata.control1.request_ack);
-			ldata_builder.set("error", frame.payload.ldata.control1.error);
-			ldata_builder.set("address_type", frame.payload.ldata.control2.address_type);
-			ldata_builder.set("hops", frame.payload.ldata.control2.hops);
-			ldata_builder.set("source", frame.payload.ldata.source);
-			ldata_builder.set("destination", frame.payload.ldata.destination);
-			ldata_builder.set("tpdu", knxclient_tpdu_to_object(isolate, frame.payload.ldata.tpdu));
-
-			// Frame
-			ObjectBuilder builder(isolate);
-			builder.set("service", frame.service);
-			builder.set("payload", ldata_builder);
-
-			args.GetReturnValue().Set(builder);
-		} else {
-			args.GetReturnValue().SetNull();
-		}
-	} else {
-		isolate->ThrowException(
-			Exception::TypeError(
-				String::NewFromUtf8(isolate, "Argument #0 needs to be a Buffer")
-			)
-		);
-	}
-}
-
-static
 Local<Value> knxclient_knx_to_object(Isolate* isolate, const knx_packet& ind) {
 	switch (ind.service) {
 		case KNX_SEARCH_REQUEST:
@@ -144,6 +75,283 @@ void knxclient_parse_knx(const FunctionCallbackInfo<Value>& args) {
 	}
 }
 
+
+static
+Local<Object> knxclient_tpdu_to_object(Isolate* isolate, const knx_tpdu& tpdu) {
+	ObjectBuilder builder(isolate);
+
+	builder.set("tpci", tpdu.tpci);
+	builder.set("seq_number", tpdu.seq_number);
+
+	switch (tpdu.tpci) {
+		case KNX_TPCI_UNNUMBERED_DATA:
+		case KNX_TPCI_NUMBERED_DATA:
+			builder.set("apci", tpdu.info.data.apci);
+			builder.set("payload", ObjectBuilder::fromData(isolate, (const char*) tpdu.info.data.payload, tpdu.info.data.length));
+			break;
+
+		case KNX_TPCI_UNNUMBERED_CONTROL:
+		case KNX_TPCI_NUMBERED_CONTROL:
+			builder.set("control", tpdu.info.control);
+			break;
+	}
+
+	return builder;
+}
+
+static
+void knxclient_parse_cemi(const FunctionCallbackInfo<Value>& args) {
+	Isolate* isolate = args.GetIsolate();
+
+	if (args.Length() > 0 && node::Buffer::HasInstance(args[0])) {
+		Handle<Value> value = args[0];
+
+		// Extract buffer details
+		const uint8_t* data = (const uint8_t*) node::Buffer::Data(value);
+		size_t len = node::Buffer::Length(value);
+
+		// Parse CEMI
+		knx_cemi_frame frame;
+		if (knx_cemi_parse(data, len, &frame)) {
+			// L_Data payload
+			ObjectBuilder ldata_builder(isolate);
+			ldata_builder.set("priority", frame.payload.ldata.control1.priority);
+			ldata_builder.set("repeat", frame.payload.ldata.control1.repeat);
+			ldata_builder.set("system_broadcast", frame.payload.ldata.control1.system_broadcast);
+			ldata_builder.set("request_ack", frame.payload.ldata.control1.request_ack);
+			ldata_builder.set("error", frame.payload.ldata.control1.error);
+			ldata_builder.set("address_type", frame.payload.ldata.control2.address_type);
+			ldata_builder.set("hops", frame.payload.ldata.control2.hops);
+			ldata_builder.set("source", frame.payload.ldata.source);
+			ldata_builder.set("destination", frame.payload.ldata.destination);
+			ldata_builder.set("tpdu", knxclient_tpdu_to_object(isolate, frame.payload.ldata.tpdu));
+
+			// Frame
+			ObjectBuilder builder(isolate);
+			builder.set("service", frame.service);
+			builder.set("payload", ldata_builder);
+
+			args.GetReturnValue().Set(builder);
+		} else {
+			args.GetReturnValue().SetNull();
+		}
+	} else {
+		isolate->ThrowException(
+			Exception::TypeError(
+				String::NewFromUtf8(isolate, "Argument #0 needs to be a Buffer")
+			)
+		);
+	}
+}
+
+void knxclient_parse_apdu(const FunctionCallbackInfo<Value>& args) {
+	Isolate* isolate = args.GetIsolate();
+
+	if (args.Length() > 1 && node::Buffer::HasInstance(args[0]) && args[1]->IsInt32()) {
+		Handle<Value> buf = args[0];
+
+		// Extract buffer details
+		const uint8_t* data = (const uint8_t*) node::Buffer::Data(buf);
+		size_t len = node::Buffer::Length(buf);
+
+		switch (args[1]->Int32Value()) {
+			case KNX_DPT_BOOL: {
+				knx_bool value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_BOOL, &value)) {
+					args.GetReturnValue().Set(value);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_CVALUE: {
+				knx_cvalue value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_CVALUE, &value)) {
+					ObjectBuilder builder(isolate);
+
+					builder.set("control", value.control);
+					builder.set("value", value.value);
+
+					args.GetReturnValue().Set(builder);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_CSTEP: {
+				knx_cstep value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_CSTEP, &value)) {
+					ObjectBuilder builder(isolate);
+
+					builder.set("control", value.control);
+					builder.set("step", value.step);
+
+					args.GetReturnValue().Set(builder);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_CHAR: {
+				knx_char value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_CHAR, &value)) {
+					args.GetReturnValue().Set(
+						String::NewFromOneByte(
+							isolate,
+							(const uint8_t*) &value,
+							String::kNormalString,
+							1
+						)
+					);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_UNSIGNED8: {
+				knx_unsigned8 value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_UNSIGNED8, &value)) {
+					args.GetReturnValue().Set(value);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_SIGNED8: {
+				knx_signed8 value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_SIGNED8, &value)) {
+					args.GetReturnValue().Set(value);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_UNSIGNED16: {
+				knx_unsigned16 value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_UNSIGNED16, &value)) {
+					args.GetReturnValue().Set(value);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_SIGNED16: {
+				knx_signed16 value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_SIGNED16, &value)) {
+					args.GetReturnValue().Set(value);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_FLOAT16: {
+				knx_float16 value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_FLOAT16, &value)) {
+					args.GetReturnValue().Set(value);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_TIMEOFDAY: {
+				knx_timeofday value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_TIMEOFDAY, &value)) {
+					ObjectBuilder builder(isolate);
+
+					builder.set("day", value.day);
+					builder.set("hour", value.hour);
+					builder.set("minute", value.minute);
+					builder.set("second", value.second);
+
+					args.GetReturnValue().Set(builder);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_DATE: {
+				knx_date value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_DATE, &value)) {
+					ObjectBuilder builder(isolate);
+
+					builder.set("day", value.day);
+					builder.set("month", value.month);
+					builder.set("year", value.year);
+
+					args.GetReturnValue().Set(builder);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_UNSIGNED32: {
+				knx_unsigned32 value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_UNSIGNED32, &value)) {
+					args.GetReturnValue().Set(value);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_SIGNED32: {
+				knx_signed32 value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_SIGNED32, &value)) {
+					args.GetReturnValue().Set(value);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			case KNX_DPT_FLOAT32: {
+				knx_float32 value;
+				if (knx_dpt_from_apdu(data, len, KNX_DPT_FLOAT32, &value)) {
+					args.GetReturnValue().Set(value);
+				} else {
+					args.GetReturnValue().SetNull();
+				}
+
+				break;
+			}
+
+			default:
+				args.GetReturnValue().SetNull();
+				break;
+		}
+	} else {
+		isolate->ThrowException(
+			Exception::TypeError(
+				String::NewFromUtf8(isolate, "Invalid argument types")
+			)
+		);
+	}
+}
+
 static
 void knxclient_init(Handle<Object> module) {
 	Isolate* isolate = Isolate::GetCurrent();
@@ -165,6 +373,34 @@ void knxclient_init(Handle<Object> module) {
 	builder.set("TunnelRequest",              KNX_TUNNEL_REQUEST);
 	builder.set("TunnelResponse",             KNX_TUNNEL_RESPONSE);
 	builder.set("RoutingIndication",          KNX_ROUTING_INDICATION);
+
+	// TPCI constants
+	builder.set("UnnumberedData",             KNX_TPCI_UNNUMBERED_DATA);
+	builder.set("NumberedData",               KNX_TPCI_NUMBERED_DATA);
+	builder.set("UnnumberedControl",          KNX_TPCI_UNNUMBERED_CONTROL);
+	builder.set("NumberedControl",            KNX_TPCI_NUMBERED_CONTROL);
+	builder.set("Connected",                  KNX_TPCI_CONTROL_CONNECTED);
+	builder.set("Disconnected",               KNX_TPCI_CONTROL_DISCONNECTED);
+	builder.set("Ack",                        KNX_TPCI_CONTROL_ACK);
+	builder.set("Error",                      KNX_TPCI_CONTROL_ERROR);
+
+	// APCI constants
+	builder.set("GroupValueRead",             KNX_APCI_GROUPVALUEREAD);
+	builder.set("GroupValueResponse",         KNX_APCI_GROUPVALUERESPONSE);
+	builder.set("GroupValueWrite",            KNX_APCI_GROUPVALUEWRITE);
+	builder.set("IndividualWrite",            KNX_APCI_INDIVIDUALADDRWRITE);
+	builder.set("IndividualRequest",          KNX_APCI_INDIVIDUALADDRREQUEST);
+	builder.set("IndividualResponse",         KNX_APCI_INDIVIDUALADDRRESPONSE);
+	builder.set("ADCRead",                    KNX_APCI_ADCREAD);
+	builder.set("ADCResponse",                KNX_APCI_ADCRESPONSE);
+	builder.set("MemoryRead",                 KNX_APCI_MEMORYREAD);
+	builder.set("MemoryResponse",             KNX_APCI_MEMORYRESPONSE);
+	builder.set("MemoryWrite",                KNX_APCI_MEMORYWRITE);
+	builder.set("UserMessage",                KNX_APCI_USERMESSAGE);
+	builder.set("MaskVersionRead",            KNX_APCI_MASKVERSIONREAD);
+	builder.set("MaskVersionResponse",        KNX_APCI_MASKVERSIONRESPONSE);
+	builder.set("Restart",                    KNX_APCI_RESTART);
+	builder.set("Escape",                     KNX_APCI_ESCAPE);
 
 	// Methods
 	builder.set("parseCEMI", knxclient_parse_cemi);
